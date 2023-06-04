b0e7a9df3117a26908f2fbd4d6ebc35c82cc6cf5
Eric Williams <ewilliams0305@gmail.com>
2023-06-04 09:52:35 -0400

    added incremental generator to create attribute

---
+++ source/WebLogger.Generators/IncrementalCommandGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Text;
+using System.Diagnostics;
+
+namespace WebLogger.Generators
+{
+    /// <summary>
+    /// Creates an IWebLogger command from a partial class with a method tagged with the CommandHandler attribute
+    /// </summary>
+    [Generator]
+    public class IncrementalCommandGenerator : IIncrementalGenerator
+    {
+        
+
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterPostInitializationOutput(PostInitializationCallback);
+        }
+
+        private static void PostInitializationCallback(IncrementalGeneratorPostInitializationContext context)
+        {
+            context.AddSource(Constants.CommandHandlerAttributeFile, Constants.CommandHandlerAttributeValue);
+        }
+    }
+}
