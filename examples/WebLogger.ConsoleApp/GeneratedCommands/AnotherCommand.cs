fd6b0a7e6ebba03c1b5fa6deebf90d9c87cedb89
Eric Williams <ewilliams0305@gmail.com>
2024-03-15 11:48:18 -0400

    updated crestron sdk

--- examples/WebLogger.ConsoleApp/GeneratedCommands/AnotherCommand.cs
+++ examples/WebLogger.ConsoleApp/GeneratedCommands/AnotherCommand.cs
@@ -2,18 +2,30 @@
 
 public partial class AnotherCommand
 {
-    [CommandHandler(
-        "Another Command",
-        "Hot Damn",
-        "We made a command from another")]
-    public ICommandResponse ExecutedMethod(string command, List<string> args)
-    {
-        return CommandResponse.Success(command, "We build this city on source generators");
-    }
+    //[CommandHandler(
+    //    "Another Command",
+    //    "Hot Damn",
+    //    "We made a command from another")]
+    //public ICommandResponse ExecutedMethod(string command, List<string> args)
+    //{
+    //    return CommandResponse.Success(command, "We build this city on source generators");
+    //}
 
-    public void StartSystem()
-    {
+    //public void StartSystem()
+    //{
 
+        
+    //}
 
-    }
+    //[CommandHandler(
+    //    "Get Lencore Level",
+    //    "Polls the lencore",
+    //    "do the thing"
+    //    )]
+    //public ICommandResponse GetLevel(string command, List<string> args)
+    //{
+
+    //}
+
+    
 }
